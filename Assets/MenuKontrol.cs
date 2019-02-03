cd69437a9c9e76479d77d39b9b8fc754f138a57f
sezgin <sezgintube64@gmail.com>
2019-02-03 17:29:33 +0300

    v0.0.4 --> update

--- Assets/MenuKontrol.cs
+++ Assets/MenuKontrol.cs
@@ -12,7 +12,8 @@ public class MenuKontrol : MonoBehaviour
 
     public void cik()
     {
-        Application.Quit();
+        Application.CancelQuit();
+
     }
 
 }
--- Assets/OyunYoneticisi.cs
+++ Assets/OyunYoneticisi.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OyunYoneticisi : MonoBehaviour
 {
@@ -14,12 +16,21 @@ public class OyunYoneticisi : MonoBehaviour
         donencember = GameObject.FindGameObjectWithTag("donencembertag");
     }
 
-
+   
     public void oyunbitti()
+    {
+        StartCoroutine(cagrilanmet());
+    }
+
+
+    IEnumerator cagrilanmet()
     {
         donencember.GetComponent<dondurme>().enabled = false;
         anacember.GetComponent<anacemberkod>().enabled = false;
         animator.SetTrigger("oyunbitti");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("anamenu");
     }
+
 
 }
